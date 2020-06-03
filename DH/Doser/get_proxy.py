f855ee873823ec862e202cb3079df2d651040b10
Rax0 <65619773+Rax0Savage@users.noreply.github.com>
2020-06-03 12:34:53 +0300

    Update get_proxy.py

--- DH/Doser/get_proxy.py
+++ DH/Doser/get_proxy.py
@@ -1,4 +1,12 @@
-import requests                                                                  
+import requests
+
 def get_proxy (url: str) -> str:
-                                                                                     a = requests.get(url)                                                            filename = "proxies.txt"                                                                                                                                          file = open(filename, 'w+')
-    file.write(a.text)                                                               file.close()                                                                                                                                                      return filename
+
+    a = requests.get(url)
+    filename = "proxies.txt"
+
+    file = open(filename, 'w+')
+    file.write(a.text)
+    file.close()
+
+    return filename
