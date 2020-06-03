de78f18477735c58868d87cff8049492435e6286
Rax0 <65619773+Rax0Savage@users.noreply.github.com>
2020-06-03 11:11:07 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+pkg install wget
+pkg install php
+pkg install python
+pkg install python2
+pip install requests
+pip install bs4
+pip install colorama
+python3 -m pip install vk
+python3 -m pip install vk_api
+python3 -m pip install colorama
+python3 -m pip install bs4
+python3 -m pip install pysocks
+python3 -m pip install prettytable
